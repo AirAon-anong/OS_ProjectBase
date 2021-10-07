e5989ba58ee9dff455fc63ba54b39e71f0b08509
LittleLunar <lunar.inthemoonllight@gmail.com>
2021-10-07 21:04:23 +0700

    version 5.0

--- studyCase01/Program.cs
+++ studyCase01/Program.cs
@@ -11,7 +11,7 @@ namespace Problem01
     {
         private object myLocker = new object();
         static long Sum_Global = 0;
-        static int G_index = 0;
+        // static int G_index = 0;
 
         public long SumGlobal // Encapsulation
         {
@@ -19,50 +19,30 @@ namespace Problem01
             set { Sum_Global = value; }
         }
 
-        public void Sum(byte[] Data_Global)
+        public void Sum(int startIndex, int stopIndex, byte[] Data_Global)
         {
-            while(G_index < 1000000000)
+            while(startIndex < stopIndex)
             {
                 lock(myLocker)
                 {
-                    if (G_index < 1000000000)
+                    if (Data_Global[startIndex] % 2 == 0)
                     {
-                        if (Data_Global[G_index] % 2 == 0)
-                        {
-                            if (G_index < 1000000000)
-                            {
-                              Sum_Global -= Data_Global[G_index];
-                            }
-
-                        }
-                        else if (Data_Global[G_index] % 3 == 0)
-                        {
-                            if (G_index < 1000000000)
-                            {
-                              Sum_Global += (Data_Global[G_index] * 2);
-                            }
-
-                        }
-                        else if (Data_Global[G_index] % 5 == 0)
-                        {
-                            if (G_index < 1000000000)
-                            {
-                              Sum_Global += (Data_Global[G_index] / 2);
-                            }
-
-                        }
-                        else if (Data_Global[G_index] % 7 == 0)
-                        {
-                            if (G_index < 1000000000)
-                            {
-                              Sum_Global += (Data_Global[G_index] / 3);
-                            }
-
-                        }
-                        Data_Global[G_index] = 0;
-                        G_index++;
+                          Sum_Global -= Data_Global[startIndex];
                     }
-
+                    else if (Data_Global[startIndex] % 3 == 0)
+                    {
+                          Sum_Global += (Data_Global[startIndex] * 2);
+                    }
+                    else if (Data_Global[startIndex] % 5 == 0)
+                    {
+                          Sum_Global += (Data_Global[startIndex] / 2);
+                    }
+                    else if (Data_Global[startIndex] % 7 == 0)
+                    {
+                          Sum_Global += (Data_Global[startIndex] / 3);
+                    }
+                    Data_Global[startIndex] = 0;
+                    startIndex++;
                 }
             }
         }
@@ -116,10 +96,10 @@ namespace Problem01
                 Console.WriteLine("Read Failed!");
             }
 
-            Thread th1 = new Thread(() => sumc.Sum(Data_Global));
-            Thread th2 = new Thread(() => sumc.Sum(Data_Global));
-            Thread th3 = new Thread(() => sumc.Sum(Data_Global));
-            Thread th4 = new Thread(() => sumc.Sum(Data_Global));
+            Thread th1 = new Thread(() => sumc.Sum(0, 250000000,Data_Global));
+            Thread th2 = new Thread(() => sumc.Sum(250000000, 500000000,Data_Global));
+            Thread th3 = new Thread(() => sumc.Sum(500000000, 750000000,Data_Global));
+            Thread th4 = new Thread(() => sumc.Sum(750000000, 1000000000,Data_Global));
 
             /* Start */
             Console.Write("\n\nWorking...");
